2da372548f0bc9ab9e30ab1dad788a5f7fbbbd48
Dennis Karungu <33949677+dennis2018@users.noreply.github.com>
2020-08-05 23:50:57 +0300

    Add files via upload

---
+++ frontend/src/data.js
@@ -0,0 +1,43 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'LIQUERS SOUTHERN COMFORT 1L',
+      category: 'Booze',
+      image: '/images/d1.jpg',
+      price: 60,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: '2',
+      name: 'Fit Shirt',
+      category: 'Booze',
+      image: '/images/d1.jpg',
+      price: 50,
+      brand: ' Nike',
+      rating: 4.2,
+      numReviews: 5
+    },
+    {
+      _id: '3',
+      name: 'Best Pants',
+      category: 'Pantry',
+      image: '/images/d1.jpg',
+      price: 70,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8
+    }, {
+      _id: '4',
+      name: 'Best Pants',
+      category: 'Pantry',
+      image: '/images/d1.jpg',
+      price: 70,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8
+    },
+  ]
+}
